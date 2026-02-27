8f5c7eb2da3b5573e6c22ccb5bcb0c3497b096a5
Aung Kyaw Phyo <kiru.kiru28@gmail.com>
2026-02-27 23:00:28 +0700

    Fix content security policy

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   config.content_security_policy do |policy|
     policy.default_src :self, :https
     policy.font_src    :self, :https, :data
-    policy.img_src :self, :https, :unsafe_inline
+    policy.img_src :self, :https, :unsafe_inline, :data
     policy.object_src  :none
     policy.script_src  :self, :https, :unsafe_inline
     policy.style_src   :self, :https, :unsafe_inline
