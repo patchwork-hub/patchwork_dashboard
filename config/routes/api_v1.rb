bf14486dcf3862bae5cbcce366e2fcaf967f0ccd
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2026-04-09 10:06:43 +0630

    feat: add change_boost_bot_profile endpoint and update related services

--- config/routes/api_v1.rb
+++ config/routes/api_v1.rb
@@ -51,6 +51,7 @@ namespace :api, defaults: { format: :json } do
         get :find_out_catch_up
         get :find_out_speak_out
         get :leicester_channels
+        patch :change_boost_bot_profile
       end
       member do
         get :starter_packs_detail
