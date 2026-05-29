e99988a7cdec637a6718fdb0d54f3d8844b055a3
Nyan Lin Htut <nyanlinhtut925@gmail.com>
2026-05-29 17:47:16 +0700

    feat: improve admin user validation, update password constraints

--- app/models/form/master_admin.rb
+++ app/models/form/master_admin.rb
@@ -3,13 +3,20 @@ class Form::MasterAdmin
 
   attr_accessor :id, :display_name, :username, :email, :password, :password_confirmation, :note, :role, :current_user
 
+  validates :username, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "format is invalid" }
+  validates :password, presence: true, confirmation: true, length: { minimum: 8 }, if: :password_required?
   validate :validate_role_assignment
+  validate :validate_username_uniqueness
+  validate :validate_email_uniqueness
 
   def initialize(attributes = {})
     super(attributes.to_h.symbolize_keys)
   end
 
   def save
+    return false unless valid?
+
     ActiveRecord::Base.transaction do
       account = create_or_update_account!
       create_or_update_user!(account)
@@ -31,19 +38,25 @@ class Form::MasterAdmin
   private
 
   def create_or_update_account!
-    Account.where(username: username).first_or_initialize.tap do |account|
-      account.assign_attributes(
-        username: username,
-        display_name: display_name,
-        note: note
-      )
-
-      unless account.valid?
-        raise ActiveModel::ValidationError.new(account)
-      end
-
-      account.save!
+    account = nil
+    if id.present?
+      user = User.find_by(id: id)
+      account = user&.account
     end
+    account ||= Account.where(username: username).first_or_initialize
+
+    account.assign_attributes(
+      username: username,
+      display_name: display_name,
+      note: note
+    )
+
+    unless account.valid?
+      raise ActiveModel::ValidationError.new(account)
+    end
+
+    account.save!
+    account
   end
 
   def create_or_update_user!(account)
@@ -54,17 +67,27 @@ class Form::MasterAdmin
                   end
     role_record ||= UserRole.find_by!(name: 'MasterAdmin')
 
-    user = User.where(email: email).first_or_initialize
+    user = nil
+    if id.present?
+      user = User.find_by(id: id)
+    end
+    user ||= User.where(email: email).first_or_initialize
+
     user.assign_attributes(
       email: email,
-      password: password,
-      password_confirmation: password_confirmation,
       confirmed_at: Time.current,
       role: role_record,
       account: account,
       agreement: true,
       approved: true
     )
+
+    if password.present?
+      user.assign_attributes(
+        password: password,
+        password_confirmation: password_confirmation
+      )
+    end
 
     unless user.valid?
       raise ActiveModel::ValidationError.new(user)
@@ -88,5 +111,40 @@ class Form::MasterAdmin
     if assigned_role && !current_user.role.overrides?(assigned_role)
       errors.add(:role, "cannot be equal to or higher than your own role")
     end
+  end
+
+  def validate_username_uniqueness
+    return if username.blank?
+
+    if id.nil?
+      if Account.exists?(username: username)
+        errors.add(:username, "has already been taken")
+      end
+    else
+      user = User.find_by(id: id)
+      existing_account = Account.find_by(username: username)
+      if existing_account && existing_account.id != user&.account_id
+        errors.add(:username, "has already been taken")
+      end
+    end
+  end
+
+  def validate_email_uniqueness
+    return if email.blank?
+
+    if id.nil?
+      if User.exists?(email: email)
+        errors.add(:email, "has already been taken")
+      end
+    else
+      existing_user = User.find_by(email: email)
+      if existing_user && existing_user.id.to_s != id.to_s
+        errors.add(:email, "has already been taken")
+      end
+    end
+  end
+
+  def password_required?
+    id.nil? || password.present?
   end
 end
