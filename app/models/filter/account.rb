c3c865c5b38014dcd1596ebbcf477a160628cc3a
Aung Kyaw Phyo <kiru.kiru28@gmail.com>
2026-06-05 17:23:39 +0700

    Refine accounts search and pagination

--- app/models/filter/account.rb
+++ app/models/filter/account.rb
@@ -1,16 +1,19 @@
 class Filter::Account < Filter::Common
 
   def initialize(params)
+    @query = params[:q].to_s.strip
     @role_id_nil = params[:role_id_nil]
-    super(params)
+    @current_page = params[:page].to_i > 0 ? params[:page].to_i : 1
+    @per_page = DEFAULT_ITEMS_LIMIT
+    @total_pages = (public_scope.count.to_f / @per_page).ceil
   end
 
   def paginated_scope
-    build_search.result.offset((@current_page - 1) * @per_page).limit(@per_page)
+    public_scope.offset((@current_page - 1) * @per_page).limit(@per_page)
   end
 
   def build_search
-    base_scope.ransack(@q)
+    public_scope
   end
 
   def get
@@ -26,5 +29,38 @@ class Filter::Account < Filter::Common
 
     scope = scope.where(users: { role_id: nil }) if @role_id_nil
     scope
+  end
+
+  def public_scope
+    scope = base_scope
+
+    return scope if @query.blank?
+
+    if exact_username_domain_query?
+      scope.where(
+        "LOWER(accounts.username) = :username AND LOWER(COALESCE(accounts.domain, '')) = :domain",
+        username: exact_username,
+        domain: exact_domain
+      )
+    else
+      search_term = "%#{ActiveRecord::Base.sanitize_sql_like(@query)}%"
+
+      scope.where(
+        "accounts.username ILIKE :term OR accounts.display_name ILIKE :term OR users.email ILIKE :term",
+        term: search_term
+      )
+    end
+  end
+
+  def exact_username_domain_query?
+    @query.match?(/\A@([^@]+)@([^@]+)\z/)
+  end
+
+  def exact_username
+    @query.match(/\A@([^@]+)@([^@]+)\z/)[1].downcase
+  end
+
+  def exact_domain
+    @query.match(/\A@([^@]+)@([^@]+)\z/)[2].downcase
   end
 end
